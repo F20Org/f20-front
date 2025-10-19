87876dfa65c12f404aa71c05099f9dfb52487a74
PedroVidalDev <pedrohvidals@gmail.com>
2025-10-19 15:55:26 -0300

    style: update GlobalStyle to set background color to white and remove padding

--- src/themes/globalStyle.ts
+++ src/themes/globalStyle.ts
@@ -27,8 +27,6 @@ export const GlobalStyle = createGlobalStyle`
         display: flex;
         width: 100%;
         height: 100vh;
-        background-color: #FAFAFA;
-
-        padding-left: 320px;
+        background-color: #FFF;
     }
 `
