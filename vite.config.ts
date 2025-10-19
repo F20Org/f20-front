cfc34c6ad89113b7c1613b296fead003e19c60d6
PedroVidalDev <pedrohvidals@gmail.com>
2025-10-19 16:17:23 -0300

    fix: update base path in Vite config to './'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import * as path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'f20-front/',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
