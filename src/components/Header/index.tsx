c14513b7a1c1f9a6626669042ccd8588b7893f55
PedroVidalDev <pedrohvidals@gmail.com>
2025-10-20 10:05:09 -0300

    feat: implement mobile menu functionality with animated options

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,12 +4,16 @@ import {
   HeaderContainer,
   IconImage,
   MenuMobileButton,
+  MobileOptions,
   Options,
   UserProfileIcon,
 } from './styles'
 import { Icon } from 'components/Icon'
+import { useState } from 'react'
 
 export const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
   return (
     <HeaderContainer>
       <IconImage src={IconLogo} />
@@ -19,9 +23,15 @@ export const Header = () => {
         <li>Homebrew</li>
       </Options>
       <UserProfileIcon />
-      <MenuMobileButton>
+      <MenuMobileButton onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
         <Icon name='IoMenu' size='32px' color='GRAY_1000' />
       </MenuMobileButton>
+
+      <MobileOptions isMobileMenuOpen={isMobileMenuOpen}>
+        <li>Fichas</li>
+        <li>Campanhas</li>
+        <li>Homebrew</li>
+      </MobileOptions>
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -34,6 +34,74 @@ export const Options = styled.ul`
     display: none;
   }
 `
+interface MobileOptionsProps {
+  isMobileMenuOpen: boolean
+}
+
+export const MobileOptions = styled.ul<MobileOptionsProps>`
+  position: absolute;
+
+  list-style: none;
+
+  top: 72px;
+  left: 0;
+  width: 100%;
+
+  z-index: 99;
+
+  display: flex;
+  flex-direction: column;
+
+  background: rgba(255, 255, 255, 0.95);
+  backdrop-filter: blur(2px);
+
+  align-items: center;
+  justify-content: center;
+
+  gap: 24px;
+  padding: 16px 0;
+
+  li {
+    font-size: ${({ theme }) => theme.FONT_SIZE.MEDIUM};
+
+    cursor: pointer;
+
+    transition: 0.5s;
+
+    &:hover {
+      color: ${({ theme }) => theme.COLORS.GRAY_300};
+    }
+  }
+
+  animation: ${({ isMobileMenuOpen }) =>
+      isMobileMenuOpen ? 'fadeIn' : 'fadeOut'}
+    0.3s ease-in-out;
+  animation-fill-mode: forwards;
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+      transform: translateY(-10px);
+    }
+
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes fadeOut {
+    0% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+
+    100% {
+      opacity: 0;
+      transform: translateY(-10px);
+    }
+  }
+`
 
 export const IconImage = styled.img`
   width: 90px;
