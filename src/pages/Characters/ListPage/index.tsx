c164850ed87f6c74ca601f60f53dea7bad31c25f
PedroVidalDev <pedrohvidals@gmail.com>
2025-10-20 01:22:46 -0300

    feat: restructure ListPage layout with MainHeader and button integration

--- src/pages/Characters/ListPage/index.tsx
+++ src/pages/Characters/ListPage/index.tsx
@@ -1,10 +1,17 @@
 import { Header } from 'components/Header'
-import { ListPageContainer } from './styles'
+import { ListPageContainer, MainHeader } from './styles'
+import { Button } from 'components/Button'
 
 export const ListPage = () => {
   return (
     <ListPageContainer>
       <Header />
+      <main>
+        <MainHeader>
+          <p> Fichas </p>
+          <Button label='Adicionar' onClick={() => {}} />
+        </MainHeader>
+      </main>
     </ListPageContainer>
   )
 }
--- src/pages/Characters/ListPage/styles.ts
+++ src/pages/Characters/ListPage/styles.ts
@@ -5,7 +5,31 @@ export const ListPageContainer = styled.div`
   height: 100%;
 
   display: flex;
+  flex-direction: column;
   position: relative;
 
   font-size: 90px;
+
+  main {
+    width: 100%;
+    height: 100%;
+
+    padding: 16px 42px;
+
+    margin-top: 48px;
+  }
+`
+
+export const MainHeader = styled.div`
+  width: 100%;
+  height: fit-content;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  p {
+    font-size: ${({ theme }) => theme.FONT_SIZE.XXXLARGE};
+    font-family: ${({ theme }) => theme.FONTS.PRIMARY};
+  }
 `
