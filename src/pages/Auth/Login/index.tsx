07d73022dc891e1b8b32475034392be8538ae54d
PedroVidalDev <pedrohvidals@gmail.com>
2025-10-20 11:09:23 -0300

    feat: implement form submission handling in Login component

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -2,13 +2,22 @@ import { Button } from 'components/Button'
 import { Input } from 'components/Input'
 
 import { FormContainer, ImageContainer, LoginContainer } from './styles'
+import { useNavigate } from 'react-router-dom'
 
 export const Login = () => {
+  const navigate = useNavigate()
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    navigate('/f20-front/characters')
+  }
+
   return (
     <LoginContainer>
       <ImageContainer />
       <FormContainer>
-        <form>
+        <form onSubmit={onSubmit}>
           <h1>Login</h1>
           <Input label='Email' placeholder='Digite seu email...' type='email' />
           <Input
