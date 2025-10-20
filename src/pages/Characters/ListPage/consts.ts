d79b8676458bd875ed7aa72e12b899e9b1cde23d
PedroVidalDev <pedrohvidals@gmail.com>
2025-10-20 01:36:09 -0300

    feat: add mocked character list for testing in ListPage

---
+++ src/pages/Characters/ListPage/consts.ts
@@ -0,0 +1,39 @@
+import type { CharacterItemProps } from 'components/CharacterItem/types'
+
+export const charactersListMocked: CharacterItemProps[] = [
+  {
+    name: 'Character 1',
+    classType: 'Warrior',
+    race: 'Human',
+  },
+  {
+    name: 'Character 2',
+    classType: 'Mage',
+    race: 'Elf',
+  },
+  {
+    name: 'Character 3',
+    classType: 'Rogue',
+    race: 'Dwarf',
+  },
+  {
+    name: 'Character 4',
+    classType: 'Paladin',
+    race: 'Human',
+  },
+  {
+    name: 'Character 5',
+    classType: 'Bard',
+    race: 'Elf',
+  },
+  {
+    name: 'Character 6',
+    classType: 'Warlock',
+    race: 'Human',
+  },
+  {
+    name: 'Character 7',
+    classType: 'Druid',
+    race: 'Orc',
+  },
+]
