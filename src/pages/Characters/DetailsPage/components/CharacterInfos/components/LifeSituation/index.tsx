b7a2572d407232a24542af0dfe31fa7c2833101b
PedroVidalDev <pedrohvidals@gmail.com>
2025-10-28 03:05:52 -0300

    fix: remove unused variable linting rule for LifeSituation component

--- src/pages/Characters/DetailsPage/components/CharacterInfos/components/LifeSituation/index.tsx
+++ src/pages/Characters/DetailsPage/components/CharacterInfos/components/LifeSituation/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import RedArrowImage from 'assets/icons/redArrow.svg'
 import LifeBorder from 'assets/icons/lifeBorder.svg'
 import RedArrowPlusImage from 'assets/icons/redPlusArrow.svg'
